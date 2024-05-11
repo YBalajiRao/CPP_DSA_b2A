ec043cc6344d239bfe25f9fbf6d6d931378d7f7d
Y. Balaji Rao <96159638+YBalajiRao@users.noreply.github.com>
2024-05-12 00:17:51 +0530

    Add files via upload

---
+++ pattern1hw.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n=;
+	int row,col;
+	for(row=1;row<=n;row++)
+	{
+		for(col=1;col<=6;col++)
+		{
+			cout<<'4'<<' ';
+		}
+		cout<<endl;
+	}
+}
